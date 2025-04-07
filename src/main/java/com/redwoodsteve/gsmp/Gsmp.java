43563a7666eb285620656f899b651a955c2f4667
Chris Landers <chrislanders700@gmail.com>
2025-04-06 22:37:39 -0700

    fix gitignore, first commit from laptop!!

--- src/main/java/com/redwoodsteve/gsmp/Gsmp.java
+++ src/main/java/com/redwoodsteve/gsmp/Gsmp.java
@@ -3,7 +3,7 @@ package com.redwoodsteve.gsmp;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Gsmp extends JavaPlugin {
-
+    // hello from laptop!
     @Override
     public void onEnable() {
         getLogger().info("HELLO GOOBERS!");
